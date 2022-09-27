17d145e608c2a8d95583418b3c0e1b7d527d3dde
Yevhenii Tomberg <yevhenii@tomberg.com>
2022-09-28 01:24:43 +0300

    Add project files.

---
+++ assignment3_yevhenii/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace assignment3_yevhenii
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_calculate = new System.Windows.Forms.Button();
+            this.btn_clear = new System.Windows.Forms.Button();
+            this.btn_exit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.input_weight = new System.Windows.Forms.TextBox();
+            this.input_height = new System.Windows.Forms.TextBox();
+            this.label_result = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_calculate
+            // 
+            this.btn_calculate.Location = new System.Drawing.Point(21, 98);
+            this.btn_calculate.Name = "btn_calculate";
+            this.btn_calculate.Size = new System.Drawing.Size(90, 30);
+            this.btn_calculate.TabIndex = 0;
+            this.btn_calculate.Text = "Calculate";
+            this.btn_calculate.UseVisualStyleBackColor = true;
+            this.btn_calculate.Click += new System.EventHandler(this.btn_calculate_Click);
+            // 
+            // btn_clear
+            // 
+            this.btn_clear.Location = new System.Drawing.Point(135, 98);
+            this.btn_clear.Name = "btn_clear";
+            this.btn_clear.Size = new System.Drawing.Size(90, 30);
+            this.btn_clear.TabIndex = 1;
+            this.btn_clear.Text = "Clear";
+            this.btn_clear.UseVisualStyleBackColor = true;
+            this.btn_clear.Click += new System.EventHandler(this.btn_clear_Click);
+            // 
+            // btn_exit
+            // 
+            this.btn_exit.Location = new System.Drawing.Point(246, 98);
+            this.btn_exit.Name = "btn_exit";
+            this.btn_exit.Size = new System.Drawing.Size(90, 30);
+            this.btn_exit.TabIndex = 2;
+            this.btn_exit.Text = "Exit";
+            this.btn_exit.UseVisualStyleBackColor = true;
+            this.btn_exit.Click += new System.EventHandler(this.btn_exit_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.label1.Location = new System.Drawing.Point(20, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 18);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Weight (kilos)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.label2.Location = new System.Drawing.Point(20, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(141, 18);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Height (centimeters)";
+            // 
+            // input_weight
+            // 
+            this.input_weight.Location = new System.Drawing.Point(201, 28);
+            this.input_weight.Name = "input_weight";
+            this.input_weight.Size = new System.Drawing.Size(135, 22);
+            this.input_weight.TabIndex = 5;
+            this.input_weight.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // input_height
+            // 
+            this.input_height.Location = new System.Drawing.Point(201, 56);
+            this.input_height.Name = "input_height";
+            this.input_height.Size = new System.Drawing.Size(135, 22);
+            this.input_height.TabIndex = 6;
+            this.input_height.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label_result
+            // 
+            this.label_result.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label_result.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label_result.Location = new System.Drawing.Point(23, 140);
+            this.label_result.Name = "label_result";
+            this.label_result.Size = new System.Drawing.Size(313, 49);
+            this.label_result.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(358, 213);
+            this.Controls.Add(this.label_result);
+            this.Controls.Add(this.input_height);
+            this.Controls.Add(this.input_weight);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_exit);
+            this.Controls.Add(this.btn_clear);
+            this.Controls.Add(this.btn_calculate);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "BMI Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_calculate;
+        private System.Windows.Forms.Button btn_clear;
+        private System.Windows.Forms.Button btn_exit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox input_weight;
+        private System.Windows.Forms.TextBox input_height;
+        private System.Windows.Forms.Label label_result;
+    }
+}
+
---
+++ assignment3_yevhenii/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace assignment3_yevhenii
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btn_calculate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double weight = Double.Parse(this.input_weight.Text);
+                double height = Double.Parse(this.input_height.Text) / 100;
+                double result = weight / (height * height);
+                if (result > 18.5 && result < 25)
+                {
+                    this.label_result.Text = "BMI: " + result + "\nStatus: normal";
+                } else if (result >= 25)
+                {
+                    this.label_result.Text = "BMI: " + result + "\nStatus: overweight";
+                } else
+                {
+                    this.label_result.Text = "BMI: " + result + "\nStatus: underweight";
+                }
+            } catch
+            {
+                MessageBox.Show("Wrong input type!", "Input Error", MessageBoxButtons.OK);
+            }
+            
+        }
+
+        private void btn_clear_Click(object sender, EventArgs e)
+        {
+            this.input_height.Text = "";
+            this.input_weight.Text = "";
+            this.label_result.Text = "";
+        }
+
+        private void btn_exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
